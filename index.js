b6255d27d0e75a8d5e73afd7d64ce28d38157a7f
fekher <fekheryahya@gmail.com>
2025-02-24 23:17:32 +0100

    feat:adding routes and models

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
-import express from "express"
-const app = express()
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import authRoute from "./routes/auth.js";
+import roomsRoute from "./routes/rooms.js";
+import hotelsRoute from "./routes/hotel.js";
+import usersRoute from "./routes/users.js";
 
+
+const app = express();
+dotenv.config();
+
+
+
+const connect = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO);
+    console.log("Connected to mongoDB.");
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+mongoose.connection.on("disconnected", () => {
+  console.log("mongoDB disconnected!");
+});
+mongoose.connection.on("connected", () => {
+  console.log("mongoDB connected!");
+});
+
+app.use("/auth", authRoute);
+app.use("/users", usersRoute);
+app.use("/rooms", roomsRoute);
+app.use("/hotel", hotelsRoute);
+
+
+// Start server after DB connection
 app.listen(8800, () => {
-    console.log("connected to backend")
-})
+  connect(); // Ensure DB connection starts when the server starts
+  console.log("Connected to backend on port 8800");
+});
